0b8972f5d9937f3e94adf2bbb621bee75ce825aa
oatman <hyperdude5329@sbcglobal.net>
2013-11-15 20:08:02 -0600

    I created a class

---
+++ dropped/Models/DRPDictionary.h
@@ -0,0 +1,13 @@
+//
+//  DRPDictionary.h
+//  dropped
+//
+//  Created by Jared Jones on 11/15/13.
+//  Copyright (c) 2013 Brad Zeis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DRPDictionary : NSObject
+
+@end
---
+++ dropped/Models/DRPDictionary.m
@@ -0,0 +1,13 @@
+//
+//  DRPDictionary.m
+//  dropped
+//
+//  Created by Jared Jones on 11/15/13.
+//  Copyright (c) 2013 Brad Zeis. All rights reserved.
+//
+
+#import "DRPDictionary.h"
+
+@implementation DRPDictionary
+
+@end

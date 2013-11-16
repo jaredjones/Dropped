7746f3fe05e7a896d5e84ae9dbf945c14b0ed14e
Brad Zeis <bradzeis@gmail.com>
2013-11-15 19:34:50 -0600

    Initial commit.

---
+++ dropped/DRPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DRPAppDelegate.h
+//  dropped
+//
+//  Created by Brad Zeis on 11/15/13.
+//  Copyright (c) 2013 Brad Zeis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DRPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ dropped/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  dropped
+//
+//  Created by Brad Zeis on 11/15/13.
+//  Copyright (c) 2013 Brad Zeis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DRPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DRPAppDelegate class]));
+    }
+}

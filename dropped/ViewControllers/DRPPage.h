76026d9fcc31c972f6ec473b7a5a12898d44444f
Brad Zeis <bradzeis@gmail.com>
2013-12-01 12:47:08 -0600

    Added DRPPage protocol.

---
+++ dropped/ViewControllers/DRPPage.h
@@ -0,0 +1,33 @@
+//
+//  DRPPage.h
+//  dropped
+//
+//  Created by Brad Zeis on 12/1/13.
+//  Copyright (c) 2013 Brad Zeis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSInteger, DRPPageID) {
+    DRPPageSplash,
+    DRPPageLogIn,
+    DRPPageMatch,
+    DRPPageGKMatchMaker,
+    DRPPageList,
+    DRPPageEtCetera,
+    DRPPageCount
+};
+
+@protocol DRPPage <NSObject>
+
+// Must "@synthesize pageID=_pageID" when conforming to DRPPage
+@property (readonly) DRPPageID pageID;
+
+@optional
+- (void)willMoveToCurrent:(NSDictionary *)userInfo;
+- (void)didMoveToCurrent;
+
+- (void)willMoveFromCurrent;
+- (void)didMoveFromCurrent;
+
+@end

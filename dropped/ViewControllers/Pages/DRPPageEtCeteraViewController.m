6e647332cfe036026a3847141544b651d0148491
Brad Zeis <bradzeis@gmail.com>
2014-02-06 16:41:20 -0600

    Added under construction image to Et Cetera page.

--- dropped/ViewControllers/Pages/DRPPageEtCeteraViewController.m
+++ dropped/ViewControllers/Pages/DRPPageEtCeteraViewController.m
@@ -11,6 +11,8 @@
 
 @interface DRPPageEtCeteraViewController ()
 
+@property UIImageView *underConstruction;
+
 @end
 
 @implementation DRPPageEtCeteraViewController
@@ -22,6 +24,28 @@
         self.topCue = @"Back";
     }
     return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    UIImage *image = [UIImage imageNamed:@"under_construction.png"];
+    _underConstruction = [[UIImageView alloc] initWithImage:image];
+    _underConstruction.frame = ({
+        CGRect frame = CGRectZero;
+        frame.size.width = image.size.width / 2;
+        frame.size.height = image.size.height / 2;
+        frame;
+    });
+    [self.scrollView addSubview:_underConstruction];
+}
+
+- (void)viewDidLayoutSubviews
+{
+    [super viewDidLayoutSubviews];
+    
+    _underConstruction.center = self.scrollView.center;
 }
 
 @end

9f437d6470568ed4189b0175f148d79470986606
Brad Zeis <bradzeis@gmail.com>
2014-03-08 19:05:09 -0600

    Added networking method stubs.

--- dropped/Networking/DRPNetworking.h
+++ dropped/Networking/DRPNetworking.h
@@ -10,4 +10,21 @@
 
 @interface DRPNetworking : NSObject
 
++ (void)generateDeviceIDWithCompletion:(void (^)())completion;
+
++ (void)aliasForDeviceID:(NSString *)deviceID withCompletion:(void (^)(NSString *))completion;
++ (void)aliasForUserID:(NSString *)userID withCompletion:(void (^)(NSString *))completion;
+
++ (void)setAlias:(NSString *)alias withCompletion:(void (^)(NSString *))completion;
+
++ (void)associateFacebook:(NSString *)userID withCompletion:(void (^)())completion;
++ (void)disassociateFacebookWithCompletion:(void (^)())completion;
++ (void)facebookFriendsWithCompletion:(void (^)(NSArray *))completion;
+
++ (void)requestMatchWithFriend:(NSString *)userID withCompletion:(void (^)(NSString *, BOOL, NSString *))completion;
++ (void)matchData:(NSString *)matchID withCompletion:(void (^)(NSData *))completion;
+
++ (void)submitMatchData:(NSData *)matchData forMatchID:(NSString *)matchID withCompletion:(void (^)())completion;
++ (void)concedeMatchID:(NSString *)matchID withCompletion:(void (^)())completion;
+
 @end
--- dropped/Networking/DRPNetworking.m
+++ dropped/Networking/DRPNetworking.m
@@ -10,4 +10,43 @@
 
 @implementation DRPNetworking
 
++ (void)generateDeviceIDWithCompletion:(void (^)())completion {
+}
+
+#pragma mark Aliases
+
++ (void)aliasForDeviceID:(NSString *)deviceID withCompletion:(void (^)(NSString *))completion {
+}
+
++ (void)aliasForUserID:(NSString *)userID withCompletion:(void (^)(NSString *))completion {
+}
+
++ (void)setAlias:(NSString *)alias withCompletion:(void (^)(NSString *))completion {
+}
+
+#pragma mark Facebook
+
++ (void)associateFacebook:(NSString *)userID withCompletion:(void (^)())completion {
+}
+
++ (void)disassociateFacebookWithCompletion:(void (^)())completion {
+}
+
++ (void)facebookFriendsWithCompletion:(void (^)(NSArray *))completion {
+}
+
+#pragma mark Matches
+
++ (void)requestMatchWithFriend:(NSString *)userID withCompletion:(void (^)(NSString *, BOOL, NSString *))completion {
+}
+
++ (void)matchData:(NSString *)matchID withCompletion:(void (^)(NSData *))completion {
+}
+
++ (void)submitMatchData:(NSData *)matchData forMatchID:(NSString *)matchID withCompletion:(void (^)())completion {
+}
+
++ (void)concedeMatchID:(NSString *)matchID withCompletion:(void (^)())completion {
+}
+
 @end
